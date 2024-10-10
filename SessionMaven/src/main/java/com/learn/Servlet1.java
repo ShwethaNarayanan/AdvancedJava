8ac392233fe8239b0f4de6d4fbd94e9b68002742
Shwetha <shweth.narayanan@gmail.com>
2024-10-10 14:59:02 -0700

    Session

---
+++ SessionMaven/src/main/java/com/learn/Servlet1.java
@@ -0,0 +1,31 @@
+package com.learn;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+@WebServlet("/servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		HttpSession session = request.getSession();
+		session.setAttribute("fname", fname);
+		session.setAttribute("lname", lname);
+		response.getWriter().append("<a href='/WebAppMaven1/servlet2'><h1> Click here <h1></href>");
+	}
+
+}
---
+++ SessionMaven/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="javax.servlet.http.HttpServlet" %>
+<html>
+<body>
+<form action="servlet1" method ="get">
+First name : <input type = "text" name = "fname">
+Last name : <input type = "text" name = "lname">
+<input type ="submit" name = "submit" >
+</form>
+</body>
+</html>
